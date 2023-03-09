725090973fce2a1ea08e2f93d346568e06b4e5fb
warith <warith.ad02@gmail.com>
2023-03-09 04:15:45 +0100

    Implement methods to handle requests of post and comments

---
+++ server/src/controllers/postController.js
@@ -0,0 +1,60 @@
+const Post = require('../models/post')
+const Comment = require('../models/comment')
+const async = require('async')
+
+exports.posts_get = async (req, res, next) => {
+    const posts = await Post.find({}, 'title').exec();
+    if(!posts){
+        res.status(500).json({message: 'Server error'});
+        next(err);
+    }
+    return res.status(200).json({posts});
+}
+
+exports.post_get = (req, res, next) =>{
+    async.parallel(
+    {
+        post(){
+            Post.findById(req.params.id)
+                .exec();
+        },
+        post_comments(){
+            Comment.find({post: req.params.id})
+                .sort({createdAt: -1})
+                .exec();
+        }   
+    },
+    (err, results) =>{
+        if(err){
+            return next(err);
+        }
+        if(results.post === null){
+           const err = new Error('Post not found');
+           err.status(404);
+           return next(err);
+        }
+        return res.status(200).json({
+            post: results.post,
+            comments: results.post_comments
+        })
+    }
+)
+}
+
+exports.comment_add_post = (req, res, next) =>{
+    const {content, commenter} = req.body;
+
+    if(!content || !commenter){
+        return res.status(400).json({message: 'Incomplete request'});
+    }
+    const comment = new Comment({content: content,
+                                post: req.params.id,
+                                commenter: commenter
+                            });
+    comment.save().then((err, savedComment) => {
+        if(err){
+            next(err);
+        }
+        return res.status(200).json({savedComment});
+    })
+}
